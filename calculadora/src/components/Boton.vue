29b6af82210c077fbac2034b584bdaee6bc0e7fb
benjamin <a23benromdor@inspedralbes.cat>
2025-03-10 23:25:12 +0100

    Test 1 Vue

---
+++ calculadora/src/components/Boton.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    numero: {
+        type: Number,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <button>{{ numero }}</button>
+</template>
+
+<style scoped>
+</style>
---
+++ calculadora/src/views/Calculadora.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+import Boton from '../components/Boton.vue';
+
+const ultimoBoton = ref('null');
+
+const botonPulsado = (numero) => {
+  ultimoBoton.value = numero;
+};
+
+</script>
+
+<template>
+  <main>
+    <h1>Calculadora</h1>
+    <section>
+      <div id="botones">
+        <Boton v-for="n in 9" :key="n" :numero="n" @click="botonPulsado(n)" ></Boton> 
+      </div>
+    </section>
+    <p>Has clicat al botó: {{ ultimoBoton }}</p>
+  </main>
+</template>
+
+<style scoped>
+#botones {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+</style>
