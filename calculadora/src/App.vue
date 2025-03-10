ada462a2fa5841aa1c1c2aaa495d074e2cd71eee
benjamin <a23benromdor@inspedralbes.cat>
2025-03-11 00:05:17 +0100

    Test 13 vue

--- calculadora/src/App.vue
+++ calculadora/src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
+
+const ultimoBoton = ref('null');
+
+const botonPulsado = (numero) => {
+  ultimoBoton.value = numero;
+};
+
 </script>
 
 <template>
-  <RouterView />
+  <main>
+    <h1>Calculadora</h1>
+    <section>
+      <div id="botones">
+        <button v-for="n in 9" :key="n" :numero="n" @click="botonPulsado(n)">{{ n }}</button>
+      </div>
+    </section>
+    <p>Has clicat al botó: {{ ultimoBoton }}</p>
+  </main>
 </template>
 
 <style scoped>
+#botones {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
 </style>
